03a964cd18b3f14d447bbe272c1b801395ef4db5
Jeremie Dimino <jdimino@janestreet.com>
2017-07-26 16:09:10 +0100

    Make script faster

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -14,35 +14,38 @@ eval `opam config env --root $OPAMROOT`
 opam update
 
 PKG_LIST=$(mktemp)
-PKG_LIST_FILTERED=$(mktemp)
-trap "rm -f $PKG_LIST $PKG_LIST_FILTERED" EXIT
+trap "rm -f $PKG_LIST $PKG_LIST" EXIT
 
-for pkg in $(opam list --depends-on jbuilder -s); do
-    # atdgen is replaced by atd
-    if [[ $pkg == atdgen ]]; then
-        echo "Skipping $pkg..."
-    else
-        echo ${pkg}...
-        ver=$(opam info -f version  $pkg)
-        dev=$(opam info -f dev-repo $pkg)
-        echo "$pkg.$ver ${#pkg} $dev" >> $PKG_LIST
-    fi
-done
-
-cat $PKG_LIST | sort -n -k2 | sort -u -k3 > $PKG_LIST_FILTERED
-pkgs=$(awk '{print $1}' $PKG_LIST_FILTERED)
+opam info -f package,version,dev-repo $(opam list --depends-on jbuilder -s) | {
+    while read key pkg; do
+        [[ $key == "package:" ]]
+        read key ver
+        [[ $key == "version:" ]]
+        read key dev
+        [[ $key == "dev-repo:" ]]
+        case $pkg in
+            # atdgen is replaced by atd
+            atdgen)
+                ;;
+            *)
+                echo "$pkg.$ver ${#pkg} $dev"
+                ;;
+        esac
+    done
+} | sort -n -k2 | sort -u -k3 > $PKG_LIST
 
 cd packages
 
 # Remove old packages
 for pkg in *; do
-    if ! grep -q "^$pkg " $PKG_LIST_FILTERED; then
+    if ! grep -q "^$pkg " $PKG_LIST; then
         echo "Deleting $pkg..."
         rm -rf $pkg
     fi
 done
 
-for pkg in $pkgs; do
+# Fetch new ones
+for pkg in $(awk '{print $1}' $PKG_LIST); do
     if  [ ! -d $pkg ]; then
         opam source $pkg
     fi
