b38d65d093717710bd4bb4bd9b7c17acc3f7a908
Jeremie Dimino <jeremie@dimino.org>
2017-09-01 19:21:07 +0100

    Update script

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -16,18 +16,23 @@ opam update
 PKG_LIST=$(mktemp)
 trap "rm -f $PKG_LIST $PKG_LIST" EXIT
 
-opam info -f package,version,dev-repo $(opam list --depends-on jbuilder -s) | {
+# Don't use "--depends-on jbuilder" as this doesn't list packages that
+# are not available on the current OS
+opam info -f package,version,dev-repo,depends $(opam list -As) | \
+    grep -B3 '^ *depends:.*\bjbuilder\b' | grep -v '^--$' | {
     while read key pkg; do
         [[ $key == "package:" ]]
         read key ver
         [[ $key == "version:" ]]
         read key dev
         [[ $key == "dev-repo:" ]]
+        read key deps
+        [[ $key == "depends:" ]]
         case $pkg in
-            # atdgen is replaced by atd
-            atdgen)
-                ;;
-            *)
+	    # atdgen is replaced by atd
+	    atdgen)
+		;;
+	    *)
                 echo "$pkg.$ver ${#pkg} $dev"
                 ;;
         esac
