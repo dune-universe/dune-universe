4269e11072b1cfc4aedc30ef1ed66b53f1c778bc
Jeremie Dimino <jeremie@dimino.org>
2019-05-10 03:00:38 +0200

    Updated packages

---
+++ packages/encore.0.3/lib/encore.ml
@@ -0,0 +1,7 @@
+module Either = Either
+module Meta = Meta
+module Bijection = Bijection
+module Lole = Lole
+module Encoder = Encoder
+module Proxy_decoder = Proxy_decoder
+module Proxy_encoder = Proxy_encoder
