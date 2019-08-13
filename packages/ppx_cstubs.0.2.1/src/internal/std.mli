7666d7d79b2e0af0cba4b8bed20a049035777d13
Jeremie Dimino <jeremie@dimino.org>
2019-08-13 03:00:51 +0200

    Updated packages

---
+++ packages/ppx_cstubs.0.2.1/src/internal/std.mli
@@ -0,0 +1,99 @@
+(* This file is part of ppx_cstubs (https://github.com/fdopen/ppx_cstubs)
+ * Copyright (c) 2018-2019 fdopen
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, with linking exception;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *)
+
+type 'a return = {return : 'b. 'a -> 'b}
+
+val with_return : ('a return -> 'a) -> 'a
+
+val finally : h:(unit -> unit) -> (unit -> 'a) -> 'a
+
+external identity : 'a -> 'a = "%identity"
+
+module Util : sig
+  open Mparsetree.Ast_cur
+
+  val error : ?loc:Ast_helper.loc -> ('a, unit, string, 'b) format4 -> 'a
+
+  val error_exn : ?loc:Ast_helper.loc -> ('a, unit, string, exn) format4 -> 'a
+
+  val safe_ascii_only : string -> string
+
+  val safe_ascii_only_ml : string -> string
+
+  val safe_cname : prefix:string -> string
+
+  val safe_mlname :
+    ?capitalize:bool -> ?nowarn:bool -> ?prefix:string -> unit -> string
+
+  val with_loc : Ast_helper.loc -> (unit -> 'a) -> 'a
+
+  val str_expr : ?loc:Ast_helper.loc -> string -> Parsetree.expression
+
+  val int_expr :
+       ?loc:Ast_helper.loc
+    -> ?attrs:Ast_helper.attrs
+    -> int
+    -> Parsetree.expression
+
+  val mk_loc : 'a -> 'a Location.loc
+
+  val mk_lid : ?loc:Ast_helper.loc -> string -> Longident.t Location.loc
+
+  val mk_pat : string -> Parsetree.pattern
+
+  val mk_ident : string -> Parsetree.expression
+
+  val mk_typc :
+       ?attrs:Ast_helper.attrs
+    -> ?l:Parsetree.core_type list
+    -> string
+    -> Parsetree.core_type
+
+  val empty_stri : unit -> Parsetree.structure_item
+
+  val marshal_to_str_expr : 'a -> Parsetree.expression
+
+  val no_warn_unused_pre406 :
+    Parsetree.structure_item -> Parsetree.structure_item
+
+  val no_warn_unused :
+    string -> Parsetree.expression -> Parsetree.structure_item
+
+  val no_c_comments : string -> string
+
+  val cloc_comment : Mparsetree.Ast_cur.Ast_helper.loc -> string
+
+  val unsuffixed_file_name : unit -> string
+
+  (* [%sig: ...] [%sigi: ...] broken for some Ocaml versions ... *)
+  val sig_from_mod_type : Parsetree.structure_item -> Parsetree.signature
+
+  val ocaml_warning : string -> Parsetree.attribute
+
+  val named_stri : string -> Parsetree.expression -> Parsetree.structure_item
+end
+
+module Result : sig
+  type ('a, 'b) result = ('a, 'b) Result.result =
+    | Ok of 'a
+    | Error of 'b
+end
+
+module Various : sig
+  val use_threads : unit -> bool
+end
---
+++ packages/routes.0.5.2/src/routes.ml
@@ -0,0 +1,114 @@
+module Routes_private = struct
+  module Util = Util
+end
+
+module Method = struct
+  type standard =
+    [ `GET
+    | `HEAD
+    | `POST
+    | `PUT
+    | `DELETE
+    | `CONNECT
+    | `OPTIONS
+    | `TRACE
+    ]
+
+  type t =
+    [ standard
+    | `Other of string
+    ]
+
+  let default = `GET
+
+  let to_string = function
+    | `GET -> "GET"
+    | `HEAD -> "HEAD"
+    | `POST -> "POST"
+    | `PUT -> "PUT"
+    | `DELETE -> "DELETE"
+    | `CONNECT -> "CONNECT"
+    | `OPTIONS -> "OPTION"
+    | `TRACE -> "TRACE"
+    | `Other s -> s
+  ;;
+
+  let compare m1 m2 = String.compare (to_string m1) (to_string m2)
+end
+
+module MethodMap = Map.Make (Method)
+
+type 'a t = 'a Parser.t
+
+module R = struct
+  type 'a t =
+    { routes : 'a Parser.t Router.t MethodMap.t
+    ; url_split : string -> string list
+    }
+
+  let create url_split routes = { routes; url_split }
+end
+
+type 'a router = 'a R.t
+
+let empty = Parser.empty
+let str = Parser.str
+let int = Parser.int
+let int32 = Parser.int32
+let int64 = Parser.int64
+let bool = Parser.bool
+let s = Parser.s
+let apply = Parser.apply
+let return = Parser.return
+
+module Infix = struct
+  include Parser.Infix
+end
+
+let with_method ?(ignore_trailing_slash = true) routes =
+  let routes = List.rev routes in
+  let f acc (m, r) =
+    let current_routes =
+      match MethodMap.find_opt m acc with
+      | None -> Router.empty
+      | Some v -> v
+    in
+    let patterns = Parser.get_patterns r in
+    MethodMap.add m (Router.add patterns (Parser.strip_route r) current_routes) acc
+  in
+  let map = List.fold_left f MethodMap.empty routes in
+  R.create (Util.split_path ignore_trailing_slash) map
+;;
+
+let one_of ?(ignore_trailing_slash = true) routes =
+  let m = Method.default in
+  with_method ~ignore_trailing_slash (List.map (fun r -> m, r) routes)
+;;
+
+let run_route routes params =
+  let rec aux routes =
+    match routes with
+    | [] -> None
+    | r :: rs ->
+      (match Parser.parse r params with
+      | Some (res, []) -> Some res
+      | _ -> aux rs)
+  in
+  aux routes
+;;
+
+let run_router t params =
+  let routes, params' = Router.feed_params t params in
+  run_route routes params'
+;;
+
+let match_with_method { R.routes; url_split } ~target ~meth =
+  let routes =
+    match MethodMap.find_opt meth routes with
+    | None -> Router.empty
+    | Some v -> v
+  in
+  run_router routes (url_split target)
+;;
+
+let match' r target = match_with_method r ~target ~meth:Method.default
