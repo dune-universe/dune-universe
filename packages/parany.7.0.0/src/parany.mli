5aab09230882f7a5e5c36ebfee5d0846815e8f9d
Jeremie Dimino <jeremie@dimino.org>
2019-08-20 03:00:41 +0200

    Updated packages

---
+++ packages/parany.7.0.0/src/parany.mli
@@ -0,0 +1,57 @@
+
+(** The [demux] function must throw [Parany.End_of_input] once it is done. *)
+exception End_of_input
+
+(** [set_shm_size nb_bytes] set the memory size (in bytes) used by each shared
+    memory region. There are two such regions created during [run].
+    The default size is 1GB. *)
+val set_shm_size: int -> unit
+
+(** [get_shm_size ()] return the current size (in bytes) of one
+    shared memory region. There are two such regions created during [run]. *)
+val get_shm_size: unit -> int
+
+(** [enable_core_pinning ()] turn ON pinning worker processes to
+    distinct cores; default is OFF. Must be called before [run].
+    Worker processes are pinned to cores [0..(nprocs-1)].
+    Don't turn this ON on a shared computer with several users, or if you
+    are running several parany-enabled jobs on the same computer. *)
+val enable_core_pinning: unit -> unit
+
+(** [disable_core_pinning ()] turn OFF core pinning. cf. [enable_core_pinning]. *)
+val disable_core_pinning: unit -> unit
+
+(** Call [set_copy_on_work ()] to turn ON data copy out of the shared memory
+    prior to each call to the [work] function. Default: OFF. *)
+val set_copy_on_work: unit -> unit
+
+(** Call [set_copy_on_mux ()] to turn ON data copy out of the shared memory
+    prior to each call to the [mux] function. Default: OFF. *)
+val set_copy_on_mux: unit -> unit
+
+(** [run ~verbose:false ~csize:10 ~nprocs:16 ~demux:f ~work:g ~mux:h] will run
+    in parallel on 16 cores the [g] function.
+    Inputs to function [g] are produced by function [f]
+    and grouped by 10 (the chunk size [csize]).
+    The demux function [f] must throw [Parany.End_of_input]
+    once it is done.
+    Outputs of function [g] are consumed by function [h].
+    Functions [f] and [g] are run by different unix processes.
+    Function [g] is run by [nprocs] processes at the same time.
+    Only function [mux] is run by the same process that called
+    [Parany.run].
+    A [csize] of one should be the default.
+    The optimal [csize] depends on your computer, the functions
+    you are using and the granularity of your computation.
+    Elements which are fast to process may benefit from
+    a [csize] greater than one.
+    In order to troubleshoot an application, run parany with [verbose]
+    set to true. This will print messages on stderr when a queue
+    is empty or full. *)
+val run:
+  verbose:bool ->
+  csize:int ->
+  nprocs:int ->
+  demux:(unit -> 'a) ->
+  work:('a -> 'b) ->
+  mux:('b -> unit) -> unit
---
+++ packages/parany.7.0.0/src/test.ml
@@ -0,0 +1,39 @@
+
+open Printf
+
+let n = 100_000
+let inputs = Array.init n (fun i -> i)
+let counter = ref 0
+
+let demux () =
+  if !counter = n then
+    raise Parany.End_of_input
+  else
+    let res = inputs.(!counter) in
+    incr counter;
+    res
+
+let work a =
+  a
+
+let res_counter = ref 0
+let results = Array.make n 0
+
+let mux x =
+  results.(!res_counter) <- x;
+  incr res_counter
+
+let main () =
+  let argc = Array.length Sys.argv in
+  if argc <> 3 then
+    (eprintf "usage: %s nprocs csize\n" Sys.argv.(0);
+     exit 1);
+  let nprocs = int_of_string Sys.argv.(1) in
+  let csize = int_of_string Sys.argv.(2) in
+  (* Parany.set_core_pinning true; *)
+  Parany.run ~verbose:true ~csize ~nprocs ~demux ~work ~mux;
+  for i = 0 to n - 1 do
+    Printf.printf "%d\n" results.(i)
+  done
+
+let () = main ()
