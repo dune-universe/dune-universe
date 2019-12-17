d85a17a16e590c7f94462b7c1b0d9ab22ab53cd1
Jeremie Dimino <jeremie@dimino.org>
2019-12-17 03:00:39 +0100

    Updated packages

--- packages/radare2.0.0.3/r2.mli
+++ packages/radare2.0.0.3/r2.mli
@@ -9,7 +9,7 @@ val command : r2:r2 -> string -> string
 (** Send a command to r2, get back Yojson. If output isn't JSON
     parsable then raises {Invalid_argument} so make sure command starts
     with /j *)
-val command_json : r2:r2 -> string -> Yojson.Basic.json
+val command_json : r2:r2 -> string -> Yojson.Basic.t
 
 (** Create a r2 instance with a given file, raises {Invalid_argument}
     if file doesn't exists *)
@@ -24,4 +24,4 @@ val with_command : cmd:string -> string -> string
 
 (** Convenience function for opening a r2 instance, sending a command,
     getting the result as Yojson and closing the r2 instance *)
-val with_command_j : cmd:string -> string -> Yojson.Basic.json
+val with_command_j : cmd:string -> string -> Yojson.Basic.t
