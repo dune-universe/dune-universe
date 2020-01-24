9979c57803b11d7934365fc730d88571983a4100
Jeremie Dimino <jeremie@dimino.org>
2020-01-24 03:00:41 +0100

    Updated packages

---
+++ packages/menhir.20200123/src/LR1Sigs.ml
@@ -0,0 +1,45 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Menhir                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*              Yann Régis-Gianas, PPS, Université Paris Diderot              *)
+(*                                                                            *)
+(*  Copyright Inria. All rights reserved. This file is distributed under the  *)
+(*  terms of the GNU General Public License version 2, as described in the    *)
+(*  file LICENSE.                                                             *)
+(*                                                                            *)
+(******************************************************************************)
+
+open Grammar
+
+(* The output signature of several LR(1) automaton construction algorithms. *)
+
+module type LR1_AUTOMATON = sig
+
+  (* An abstract type of nodes, that is, states in the LR(1) automaton. *)
+
+  type node
+
+  (* The number of nodes. *)
+
+  val n: int
+
+  (* Nodes are numbered from 0 to [n-1]. *)
+
+  val number: node -> int
+  val node: int -> node
+
+  (* To each start production corresponds an entry node. *)
+
+  val entry : node ProductionMap.t
+
+  (* Each node carries outgoing transitions towards other nodes. *)
+
+  val transitions: node -> node SymbolMap.t
+
+  (* Each node represents an LR(1) state, that is, a set of LR(1) items. *)
+
+  val state: node -> Lr0.lr1state
+
+end
