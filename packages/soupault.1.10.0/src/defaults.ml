b41dfb93bc336490db8bde5de11827632d5e3c58
Jeremie Dimino <jeremie@dimino.org>
2020-03-25 03:00:49 +0100

    Updated packages

---
+++ packages/soupault.1.10.0/src/defaults.ml
@@ -0,0 +1,190 @@
+(** global settings that can be updated from config file or command line options *)
+type index_field = {
+  field_name : string;
+  field_selector : string;
+  select_all : bool;
+  default_field_value : string option
+}
+
+type index_processor = BuiltInTemplate of Mustache.t | ExternalIndexer of string
+
+type index_view = {
+  index_view_name : string;
+  index_selector : string;
+  index_processor : index_processor;
+}
+
+type path_options = {
+  pages: string list;
+  sections: string list;
+  regexes: string list;
+  pages_exclude: string list;
+  sections_exclude: string list;
+  regexes_exclude: string list;
+}
+
+type settings = {
+  (* show processing steps *)
+  verbose : bool;
+
+  (* show debug information *)
+  debug : bool;
+
+  (* stop on page processing errors *)
+  strict : bool;
+
+  (* HTML doctype to insert in generated pages *)
+  doctype : string;
+
+  (* where generated pages are stored *)
+  build_dir : string;
+
+  (* where page source files are stored *)
+  site_dir : string;
+
+  (* page source file to use for section index, without extension *)
+  index_page : string;
+
+  (* generated section index file name *)
+  index_file : string;
+
+  (* default HTML page template *)
+  default_template : string;
+
+  (* element where page content is inserted in the template *)
+  content_selector : string;
+
+  (* use clean URLs or mirror the site dir structure exactly *)
+  clean_urls : bool;
+
+  (* Extensions of files assumed to be pages *)
+  page_extensions : string list;
+
+  (* File extensions to ignore completely *)
+  ignore_extensions : string list;
+
+  (* Extension to use for page files with non-standard extensions. *)
+  default_extension : string;
+
+  (* What extensions to consider "standard" for the purpose of setting extensions.
+     That's for people who want to use Markdown etc. _without_ also using clean URLs,
+     so that about.htm remains about.htm, but contact.md becomes contact.html
+   *)
+  keep_extensions : string list;
+
+  (* Pages that should be just run through the widgets
+      rather than inserted in the template *)
+  complete_page_selector : string;
+
+  (* If set to false, soupault doesn't use or require a page template,
+     but treats everything as a complete page *)
+  generator_mode : bool;
+
+  build_profile : string option;
+
+  (* Only extract the site index, don't generate any pages *)
+  index_only : bool;
+
+  index : bool;
+  dump_json : string option;
+  newest_entries_first : bool;
+  index_title_selector : string list;
+  index_excerpt_selector : string list;
+  index_date_selector : string list;
+  index_author_selector : string list;
+  index_date_format : string;
+  ignore_template_errors : bool;
+  index_custom_fields : index_field list;
+  index_extract_after_widgets : string list;
+  index_strip_tags : bool;
+  index_views: index_view list;
+  index_path_options: path_options;
+  index_profile: string option;
+
+  preprocessors : (string * string) list;
+
+  plugin_dirs : string list;
+  plugin_discovery: bool;
+}
+
+type env = {
+  template : string;
+  nav_path : string list;
+  page_file : string;
+  page_url : string;
+  target_dir : string;
+}
+
+let config_file = "soupault.conf"
+let config_path_env_var = "SOUPAULT_CONFIG"
+let settings_table = "settings"
+let index_settings_table = "index"
+let preprocessors_table = "preprocessors"
+let widgets_table = "widgets"
+let plugins_table = "plugins"
+
+let default_index_item_template = "<div> <a href=\"{{url}}\">{{{title}}}</a> </div>"
+
+let default_index_processor = BuiltInTemplate (Mustache.of_string default_index_item_template)
+
+let default_path_options = {
+  pages = [];
+  sections = [];
+  regexes = [];
+  pages_exclude = [];
+  sections_exclude = [];
+  regexes_exclude = [];
+}
+
+let default_settings = {
+  verbose = false;
+  debug = false;
+  strict = true;
+  doctype = "<!DOCTYPE html>";
+  build_dir = "build";
+  site_dir = "site";
+  index_page = "index";
+  index_file = "index.html";
+  default_template = "templates/main.html";
+  content_selector = "body";
+  clean_urls = true;
+  page_extensions = ["htm"; "html"; "md"; "rst"; "adoc"];
+  ignore_extensions = [];
+  default_extension = "html";
+  keep_extensions = ["html"; "htm"];
+  complete_page_selector = "html";
+  generator_mode = true;
+  build_profile = None;
+
+  index = false;
+  index_only = false;
+  dump_json = None;
+  newest_entries_first = false;
+  index_title_selector = ["h1"];
+  index_excerpt_selector = ["p"];
+  index_date_selector = ["time"];
+  index_author_selector = ["#author"];
+  index_date_format = "%F";
+  ignore_template_errors = false;
+  index_extract_after_widgets = [];
+  index_custom_fields = [];
+  index_strip_tags = false;
+  index_views = [];
+  index_path_options = default_path_options;
+  index_profile = None;
+
+  preprocessors = [];
+
+  plugin_dirs = ["plugins"];
+  plugin_discovery = true;
+}
+
+let version = (1, 10, 0, None)
+
+let version_to_string v =
+  let major, minor, patch, suffix = v in
+  match suffix with
+  | Some suffix -> Printf.sprintf "%d.%d.%d-%s" major minor patch suffix
+  | None -> Printf.sprintf "%d.%d.%d" major minor patch
+
+let version_string = version_to_string version
---
+++ packages/soupault.1.10.0/src/soupault.ml
@@ -0,0 +1,394 @@
+open Defaults
+
+module FU = FileUtil
+module FP = FilePath
+
+(* Result monad *)
+let (>>=) = Stdlib.Result.bind
+let (let*) = (>>=)
+
+let mkdir dir =
+  (* Note: FileUtil.mkdir returns success if the directory
+     already exists, this is why it's not checked before creation. *)
+  try Ok (FU.mkdir ~parent:true dir)
+  with FileUtil.MkdirError e -> Error e
+
+(*** Logging setup ***)
+
+(* Omit the executable name from the logs, the user knows already *)
+let pp_header ppf (l, h) =
+  match h with
+  | None -> if l = Logs.App then () else Format.fprintf ppf "[%a] " Logs.pp_level l
+  | Some h -> Format.fprintf ppf "[%s] " h
+
+let log_reporter = Logs.format_reporter ~pp_header:pp_header  ()
+
+let setup_logging verbose debug =
+  let level =
+    if debug then Logs.Debug
+    else if verbose then Logs.Info
+    else Logs.Warning
+  in
+  Logs.set_level (Some level);
+  Logs.set_reporter log_reporter
+
+(*** Filesystem stuff ***)
+let (+/) left right =
+    FP.concat left right
+
+let list_dirs path =
+    FU.ls path |> FU.filter FU.Is_dir
+
+let remove_ignored_files settings files =
+  let ignored settings file = Utils.in_list settings.ignore_extensions (Utils.get_extension file) in
+  List.filter (fun f -> not (ignored settings f)) files
+
+let list_section_files settings path =
+  let is_page_file f = Utils.in_list settings.page_extensions (Utils.get_extension f) in
+  let files = FU.ls path |> FU.filter (FU.Is_file) |> remove_ignored_files settings in
+  let page_files = List.find_all is_page_file files in
+  let other_files = List.find_all (fun f -> not (is_page_file f)) files in
+  page_files, other_files
+
+let make_build_dir build_dir =
+  if (FU.test FU.Exists build_dir) then Ok () else
+  let () = Logs.info @@ fun m -> m "Build directory \"%s\" does not exist, creating" build_dir in
+  mkdir build_dir
+
+(** Produces a target directory name for the page.
+
+    If clean URLs are used, then a subdirectory matching the page name
+    should be created inside the section directory, unless the page is
+    a section index page.
+    E.g. "site/foo.html" becomes "build/foo/index.html" to provide
+    a clean URL.
+
+    If clean URLs are not used, only section dirs are created.
+ *)
+let make_page_dir_name settings target_dir page_name =
+  if (page_name = settings.index_page) || (not settings.clean_urls) then target_dir
+  else target_dir +/ page_name
+
+let load_html settings file =
+  let ext = Utils.get_extension file in
+  let preprocessor = CCList.assoc_opt ~eq:(=) ext settings.preprocessors in
+  try
+    match preprocessor with
+    | None -> Ok (Soup.read_file file |> Soup.parse)
+    | Some prep ->
+      let prep_cmd = Printf.sprintf "%s %s" prep file in
+      let () = Logs.info @@ fun m -> m "Calling preprocessor %s on page %s" prep file in
+      Utils.get_program_output prep_cmd [| |] >>= (fun h -> Ok (Soup.parse h))
+  with Sys_error e -> Error e
+
+let save_html settings soup file =
+  try
+    let html_str = Soup.pretty_print soup in
+    let chan = open_out file in
+    (* lambdasoup doesn't include the doctype even if it was present
+       in the source, so we have to do it ourselves *)
+    settings.doctype |> String.trim |> Printf.fprintf chan "%s\n";
+    Soup.write_channel chan html_str;
+    close_out chan;
+    Ok ()
+  with Sys_error e -> Error e
+
+let include_content settings html content =
+  let element = Soup.select_one settings.content_selector html in
+  match element with
+  | Some element -> Ok (Soup.append_child element content)
+  | None ->
+    Error (Printf.sprintf "No element in the template matches selector \"%s\", nowhere to insert the content"
+           settings.content_selector)
+
+let make_page env settings content =
+  (* If generator mode is off, treat everything like a complete page *)
+  if not settings.generator_mode then Ok content else
+  let page_wrapper_elem = Soup.select_one settings.complete_page_selector content in
+  (* If page file appears to be a complete page rather than a page body,
+     just return it *)
+  match page_wrapper_elem with
+  | Some _ ->
+    let () =
+      if settings.generator_mode then
+      Logs.debug @@ fun m -> m "File appears to be a complete page, not using the page template"
+      (* in HTML processor mode that's implied *)
+    in Ok content
+  | None ->
+    let html = Soup.parse env.template in
+    let* () = include_content settings html content in
+    Ok html
+
+(* Widget processing *)
+let rec process_widgets settings env ws wh config soup =
+  match ws with
+  | [] -> Ok ()
+  | w :: ws' ->
+    begin
+      let open Widgets in
+      let widget = Hashtbl.find wh w in
+      let () = Logs.info @@ fun m -> m "Processing widget %s on page %s" w env.page_file in
+      if not (widget_should_run w widget settings.build_profile settings.site_dir env.page_file)
+      then (process_widgets settings env ws' wh config soup) else
+      let res =
+        try widget.func env widget.config soup
+        with Utils.Soupault_error s -> Error s
+      in
+      (* In non-strict mode, widget processing errors are tolerated *)
+      match res, settings.strict with
+      | Ok _, _ -> process_widgets settings env ws' wh config soup
+      | Error _ as err, true -> err
+      | Error msg, false ->
+        let () = Logs.warn @@ fun m -> m "Processing widget \"%s\" failed: %s" w msg in
+        process_widgets settings env ws' wh config soup
+    end
+
+(** Removes index page's parent dir from its navigation path
+
+    When clean URLs are used, the "navigation path" as in the path
+    before the page doesn'a match the "real" path for index pages,
+    and if you try to use it for breadcrumbs for example,
+    section index pages will have links to themselves,
+    since the parent of foo/bar/index.html is technically "bar".
+    The only way to deal with it I could find is to remove the
+    last parent if the page is an index page.
+ *)
+let fix_nav_path settings path page_name =
+  if page_name = settings.index_page then Utils.drop_tail path
+  else path
+
+let make_page_url settings nav_path orig_path page_file =
+  let page_file_name = FP.basename page_file in
+  let page =
+    if settings.clean_urls then page_file_name |> FP.chop_extension
+    else page_file_name
+  in
+  let path =
+    if ((FP.chop_extension page_file_name) = settings.index_page) then orig_path
+    else (List.append nav_path [page])
+  in
+  (* URL path should be absolute *)
+  String.concat "/" path |> Printf.sprintf "/%s"
+
+(** Decide on the page file name.
+
+    If clean URLs are used, it's always <target_dir>/<settings.index_file>
+
+    If clean URLs are not used, then the base file name is preserved.
+    The extension, however, is set to settings.default_extension,
+    unless it's in the settings.keep_extensions list.
+
+    The reason for this extension juggling is that people may use page preprocessors
+    but not use clean URLs, without extension mangling they will end up
+    with pages like build/about.md that have HTML inside despit their name.
+    In short, that's what Jekyll et al. always did to non-blog pages.
+ *)
+let make_page_file_name settings env target_dir =
+  if settings.clean_urls then (target_dir +/ settings.index_file) else
+  let page_file = FP.basename env.page_file in
+  let extension = Utils.get_extension page_file in
+  let page_file =
+    if Utils.in_list settings.keep_extensions extension then page_file
+    else FP.add_extension (FP.chop_extension page_file) settings.default_extension
+  in target_dir +/ page_file
+
+(** Processes a page:
+
+    1. Adjusts the path to account for index vs non-index page difference
+       in setups using clean URLs
+    2. Reads a page file and inserts the content into the template,
+       unless it's a complete page
+    3. Updates the global index if necessary
+    4. Runs the page through widgets
+    5. Inserts the index section into the page if it's an index page
+    6. Saves the processed page to file
+  *)
+let process_page env index widgets config settings =
+  let page_name = FP.basename env.page_file |> FP.chop_extension in
+  let target_dir = make_page_dir_name settings env.target_dir page_name in
+  let target_file = make_page_file_name settings env target_dir in
+  let orig_path = env.nav_path in
+  let nav_path = fix_nav_path settings env.nav_path page_name in
+  let env = {env with
+    nav_path = nav_path;
+    page_url = (make_page_url settings nav_path orig_path env.page_file);
+    target_dir = target_dir
+  } in
+  let () = Logs.info @@ fun m -> m "Processing page %s" env.page_file in
+  let* content = load_html settings env.page_file in
+  let* html = make_page env settings content in
+  (* Section index injection always happens before any widgets have run *)
+  let* () =
+    (* Section index is inserted only in index pages *)
+    if (not settings.index) || (page_name <> settings.index_page) ||
+       settings.index_only  || (!index = [])
+    then Ok ()
+    else let () = Logs.info @@ fun m -> m "Inserting section index" in
+    Autoindex.insert_indices settings html !index
+  in
+  let before_index, after_index, widget_hash = widgets in
+  let* () = process_widgets settings env before_index widget_hash config html in
+  (* Index extraction *)
+  let* () =
+    (* Metadata is only extracted from non-index pages *)
+    if (not settings.index) || (page_name = settings.index_page) || not (Autoindex.index_extraction_should_run settings env.page_file) then Ok () else
+    let () =
+      let () = Logs.info @@ fun m -> m "Extracting page metadata" in
+      index := (Autoindex.get_entry settings env html) :: !index
+    in Ok ()
+  in
+  if settings.index_only then Ok () else
+  let* () = process_widgets settings env after_index widget_hash config html in
+  let* () = mkdir target_dir in
+  let* () = save_html settings html target_file in
+  Ok ()
+
+(* Monadic wrapper for process_page that can either return or ignore errors  *)
+let _process_page env index widgets config settings target_dir page_file =
+    (* Make the page file name accessible to widgets *)
+    let env = {env with page_file=page_file; target_dir=target_dir} in
+    let res = process_page env index widgets config settings in
+    match res with
+      Ok _ -> Ok ()
+    | Error msg ->
+      let msg = Printf.sprintf "Could not process page %s: %s" page_file msg in
+      if settings.strict then Error msg else 
+      let () = Logs.warn @@ fun m -> m "%s" msg in
+      Ok ()
+
+(* Reorders the pages so that the index page is processed last,
+   when the section index is available *)
+let reorder_pages settings ps =
+  let find_index = fun p -> (FP.basename p |> FP.chop_extension) = settings.index_page in
+  let index_page = CCList.find_opt find_index ps in
+  match index_page with
+  | None -> ps
+  | Some p ->
+    let ps = CCList.remove ~eq:(=) ~key:p ps in
+    List.append ps [p]
+
+(** If index file path is configured, add section index to the global index
+   that will be saved to the file *)
+let save_index settings section_index index =
+  match settings.dump_json with
+  | None -> ()
+  | Some _ -> index := List.append !section_index !index
+
+(* Process the source directory recursively
+   
+ *)
+let rec process_dir env index widgets config settings base_src_dir base_dst_dir dirname =
+  let section_index = ref [] in
+  let src_path = base_src_dir +/ dirname in
+  let dst_path = base_dst_dir +/ dirname in
+  let () = Logs.info @@ fun m -> m "Entering directory %s" src_path in
+  let nav_path = if dirname <> "" then List.append env.nav_path [dirname] else env.nav_path in
+  let env = {env with nav_path = nav_path} in
+  let pages, assets = list_section_files settings src_path in
+  let pages = reorder_pages settings pages in
+  let* () =
+    if not settings.index_only then
+      begin
+        let* () = mkdir dst_path in
+        let* () = Utils.cp assets dst_path in
+        Ok ()
+      end
+    else Ok ()
+  in
+  let dirs = List.map (FP.basename) (list_dirs src_path) in
+  let* () = Utils.iter (_process_page env section_index widgets config settings dst_path) pages in
+  let () = save_index settings section_index index in
+  Utils.iter (process_dir env index widgets config settings src_path dst_path) dirs
+
+(* Option parsing and initialization *)
+
+let get_args settings =
+  let init = ref false in
+  let sr = ref settings in
+  let args = [
+    ("--init", Arg.Unit (fun () -> init := true), "Setup basic directory structure");
+    ("--verbose", Arg.Unit (fun () -> sr := {!sr with verbose=true}), "Verbose output");
+    ("--debug", Arg.Unit (fun () -> sr := {!sr with debug=true}), "Debug output");
+    ("--strict", Arg.Bool (fun s -> sr := {!sr with strict=s}), "<true|false> Stop on page processing errors or not");
+    ("--site-dir", Arg.String (fun s -> sr := {!sr with site_dir=s}), "Directory with input files");
+    ("--build-dir", Arg.String (fun s -> sr := {!sr with build_dir=s}), "Output directory");
+    ("--profile", Arg.String (fun s -> sr := {!sr with build_profile=(Some s)}), "Build profile");
+    ("--index-only", Arg.Unit (fun () -> sr := {!sr with index_only=true}), "Extract site index without generating pages");
+    ("--version", Arg.Unit (fun () -> Utils.print_version (); exit 0), "Print version and exit")
+  ]
+  in let usage = Printf.sprintf "Usage: %s [OPTIONS]" Sys.argv.(0) in
+  let () = Arg.parse args (fun _ -> ()) usage in
+  if !init then (Project_init.init !sr; exit 0) else Ok !sr
+
+let check_project_dir settings =
+  if (not (FU.test FU.Exists settings.default_template)) && settings.generator_mode
+  then Logs.warn @@ fun m -> m "Default template %s does not exist" settings.default_template;
+  if (not (FU.test FU.Is_dir settings.site_dir))
+  then begin
+    Logs.warn @@ fun m -> m "Site directory %s does not exist" settings.site_dir;
+    Logs.warn @@ fun m -> m "Use %s --init to initialize a basic project" Sys.argv.(0);
+    exit 1
+  end
+
+let initialize () =
+  let () = Random.self_init () in
+  let settings = Defaults.default_settings in
+  let () = setup_logging settings.verbose settings.debug in
+  let config_file =
+    try Unix.getenv Defaults.config_path_env_var
+    with Not_found -> Defaults.config_file
+  in
+  let* config = Config.read_config config_file in
+  let settings = Config.update_settings settings config in
+  let* settings = get_args settings in
+  (* Update the log level from the config and arguments  *)
+  let () = setup_logging settings.verbose settings.debug in
+  let () = check_project_dir settings in
+  let* plugins = Plugins.get_plugins config in
+  let* widgets = Widgets.get_widgets settings config plugins settings.index_extract_after_widgets in
+  let* default_template_str =
+    if settings.generator_mode then Utils.get_file_content settings.default_template
+    else Ok ""
+  in
+  let default_env = {
+    template = default_template_str;
+    nav_path = [];
+    page_file = "";
+    page_url = "";
+    target_dir = settings.build_dir
+  } in
+  let () =
+    begin
+      let () = if not settings.generator_mode then
+        Logs.info @@ fun m -> m "Running in HTML processor mode, not using the page template"
+      in
+      let () = if settings.index_only && not (settings.index && (settings.dump_json <> None)) then
+        Logs.warn @@ fun m -> m "--index-only is useless without index=true and dump_json options in the config!"
+      in ()
+    end
+  in Ok (config, widgets, settings, default_env)
+
+let dump_index_json settings index =
+  match settings.dump_json with
+  | None -> Ok ()
+  | Some f ->
+    try Ok (Soup.write_file f @@ Autoindex.json_string_of_entries settings index)
+    with Sys_error e -> Error e
+  
+let main () =
+  let* config, widgets, settings, default_env = initialize () in
+  let () = setup_logging settings.verbose settings.debug in
+  let* () = make_build_dir settings.build_dir in
+  let index = ref [] in
+  let* () = process_dir default_env index widgets config settings settings.site_dir settings.build_dir "" in
+  let* () = dump_index_json settings !index in
+  Ok ()
+
+let () =
+  let res = main () in
+  match res with
+  | Ok _ -> exit 0
+  | Error e ->
+    Logs.err @@ fun m -> m "%s" e;
+    exit 1
+
