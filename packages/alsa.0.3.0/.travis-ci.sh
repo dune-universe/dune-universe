08a76544067f4e8770db5554cd3cfea619f06b64
Jeremie Dimino <jeremie@dimino.org>
2020-08-10 03:00:54 +0200

    Updated packages

---
+++ packages/alsa.0.3.0/.travis-ci.sh
@@ -0,0 +1,15 @@
+# Hacking the build into Travis-CI "C" environment
+# See http://anil.recoil.org/2013/09/30/travis-and-ocaml.html
+
+export OPAMYES=1
+opam init
+if [ -n "${OPAM_SWITCH}" ]; then
+    opam switch ${OPAM_SWITCH}
+fi
+eval `opam config env`
+opam install -y depext dune
+opam pin -y add --no-action .
+opam depext -y -i alsa 
+
+# compile
+dune build
