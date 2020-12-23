002b800f9f7fba576357d3e3101645097aa583ef
Jeremie Dimino <jeremie@dimino.org>
2020-12-23 03:01:03 +0100

    Updated packages

---
+++ packages/catala.0.2.0/syntax_highlighting/fr/setup_vscode.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+FILE=~/.vscode/extensions/catala-fr
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+if [ ! -L "$FILE" ]; then
+  echo "Creating link"
+  ln -s -f $SCRIPTPATH/vscode "$FILE"
+fi
